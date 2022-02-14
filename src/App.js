f2f97a6afd67009c9c6e0b75032cf17d4fe44fdd
brendajanos <brenda.janos@icloud.com>
2022-02-14 17:06:38 +0100

    theme switch toggle createt

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React, { useEffect, useContext } from "react"
 import CountdownPage from "./components/CountdownPage"
 import SetPomodoro from "./components/SetPomodoro"
 import { SettingsContext } from "./context/SettingsContext"
+import useLocalStorage from "use-local-storage"
+
 
 const App = () => {
-  const {
-    pomodoro,
-    executing,
-    startAnimate,
-    updateExecute,
-  } = useContext(SettingsContext)
+  const { pomodoro, executing, startAnimate, updateExecute } = useContext(SettingsContext)
+
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+  const [theme, setTheme] = useLocalStorage("theme", defaultDark ? "dark" : "light")
 
   useEffect(() => {
     updateExecute(executing)
   }, [executing, startAnimate, updateExecute])
 
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light"
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="container">
-      {pomodoro !== 0 ? (
-       <CountdownPage/>
-      ) : (
-        <SetPomodoro />
-      )}
+    <div className="container" data-theme={theme}>
+      {pomodoro !== 0 ? <CountdownPage /> : <SetPomodoro />}
+      <button className="toggle" onClick={switchTheme}>{theme === "light" ? "🌙" : "🔆"}</button>
     </div>
   )
 }
